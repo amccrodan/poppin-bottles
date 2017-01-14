ce38be920d17eff4a62e1eca65bcf0690ebcc8c7
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-14 01:37:29 +0000

    Changed output to include number of bottles gained from recycling, and leftovers.

--- poppin-bottles.js
+++ poppin-bottles.js
@@ -8,6 +8,9 @@ function poppinBottles(initialBottles, currentEmpties, currentCaps) {
   poppinBottles.bottleDepot.backFromEmpties += Math.floor(initialBottles / 2);
   poppinBottles.bottleDepot.backFromCaps += Math.floor(initialBottles / 4);
 
+  poppinBottles.bottleDepot.leftEmpties = initialBottles % 2;
+  poppinBottles.bottleDepot.leftCaps = initialBottles % 4;
+
   poppinBottles(Math.floor(initialBottles / 2) + Math.floor(initialBottles / 4),
     initialBottles % 2, initialBottles % 4);
 }
@@ -15,9 +18,19 @@ function poppinBottles(initialBottles, currentEmpties, currentCaps) {
 poppinBottles.bottleDepot = {
   fullBottles: 0,
   backFromEmpties: 0,
-  backFromCaps: 0
+  backFromCaps: 0,
+  currentEmpties: 0,
+  currentCaps: 0,
+  leftEmpties: 0,
+  leftCaps: 0
 }
 
 // $20
 poppinBottles(Number(myInput)/2);
-console.log(poppinBottles.bottleDepot);
+console.log("You will get " + poppinBottles.bottleDepot.fullBottles + " bottles total.");
+console.log("This includes " + poppinBottles.bottleDepot.backFromEmpties + " bottles from empties, and " +
+  poppinBottles.bottleDepot.backFromCaps + " from caps.");
+console.log("You have " + poppinBottles.bottleDepot.leftEmpties + " empty bottle(s) and " +
+  poppinBottles.bottleDepot.leftCaps + " cap(s) left.");
+
+// 10 bottles - 5 + 2 bottles - 3 + 1 bottles - 2 + 1 bottles - 1 bottle
